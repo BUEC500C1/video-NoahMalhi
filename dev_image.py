b6a11364caff5560702ffcb5f40932a4f791b9ce
NoahMalhi <malhin@bu.edu>
2020-02-28 16:46:31 -0500

    fix

--- dev_image.py
+++ dev_image.py
@@ -8,7 +8,7 @@ def dev_video(username, success):
     #each process runs ffmpeg using its own username.png files
     if (success == 1): 
         pid = os.getpid()
-        os.system('ffmpeg.exe -framerate 0.5 -i '+username+'%d.png video'+str(pid)+'.avi')
+        os.system('ffmpeg.exe -framerate 0.33 -i '+username+'%d.png video'+str(pid)+'.avi')
     else:
         return
 
